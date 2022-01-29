e3bd0624ae32da9f1af692a5e31efd26e4e715b1
austinmdngo <austinmdngo@gmail.com>
2022-01-29 17:04:54 -0600

    slider test

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,13 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 
-function App() {
-  return <Button variant="contained">Hello World</Button>;
+<Slider
+  defaultValue={30}
+  sx={{
+    width: 300,
+    color: 'success.main',
+    '& .MuiSlider-thumb': {
+      borderRadius: '1px',
+    },
+  }}
+/>
