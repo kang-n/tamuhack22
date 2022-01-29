2c1ba43cd1a19a27142f139708fea5752a0b93f3
austinmdngo <austinmdngo@gmail.com>
2022-01-29 17:08:18 -0600

    slider test2

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
+import { alpha, styled } from '@mui/material/styles';
 
-<Slider
-  defaultValue={30}
-  sx={{
-    width: 300,
-    color: 'success.main',
-    '& .MuiSlider-thumb': {
-      borderRadius: '1px',
+const SuccessSlider = styled(Slider)(({ theme }) => ({
+  width: 300,
+  color: theme.palette.success.main,
+  '& .MuiSlider-thumb': {
+    '&:hover, &.Mui-focusVisible': {
+      boxShadow: `0px 0px 0px 8px ${alpha(theme.palette.success.main, 0.16)}`,
     },
-  }}
-/>
+    '&.Mui-active': {
+      boxShadow: `0px 0px 0px 14px ${alpha(theme.palette.success.main, 0.16)}`,
+    },
+  },
+}));
+
+export default function StyledCustomization() {
+  return <SuccessSlider defaultValue={30} />;
+}
